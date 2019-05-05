da86c353436779f96e15e6c56e82ce64e58c7b86
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2019-05-05 23:15:37 +0530

    Updates

---
+++ precache-manifest.712823adf9644c15519d23119dec3c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a139385c18a340242959fc07294f35b0",
+    "url": "/cricviz/index.html"
+  },
+  {
+    "revision": "4efb6596d5dc7a0fbad2",
+    "url": "/cricviz/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "269776b3422aaf65d849",
+    "url": "/cricviz/static/js/2.9ff4566c.chunk.js"
+  },
+  {
+    "revision": "4efb6596d5dc7a0fbad2",
+    "url": "/cricviz/static/js/main.890d1815.chunk.js"
+  },
+  {
+    "revision": "c351f01f5c27efbb630b",
+    "url": "/cricviz/static/js/runtime~main.7d33cc5e.js"
+  }
+]);
