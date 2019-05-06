6f9a565681e5fc0da8c90f1c5248a4fd0364b2ce
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2019-05-07 00:01:50 +0530

    Updates

---
+++ precache-manifest.4161fc677b7c8be310c8762f0889a9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8163fee696837dfc27f93fe55a344c92",
+    "url": "/cricviz/first-innings/index.html"
+  },
+  {
+    "revision": "d5aaf2da23acec8a151b",
+    "url": "/cricviz/first-innings/static/css/main.90d0d30d.chunk.css"
+  },
+  {
+    "revision": "a6ef65d06dd24c6bf4df",
+    "url": "/cricviz/first-innings/static/js/2.e91c4d6b.chunk.js"
+  },
+  {
+    "revision": "d5aaf2da23acec8a151b",
+    "url": "/cricviz/first-innings/static/js/main.e1ad06d1.chunk.js"
+  },
+  {
+    "revision": "6a66fa27fdedcec35088",
+    "url": "/cricviz/first-innings/static/js/runtime~main.99274029.js"
+  }
+]);
